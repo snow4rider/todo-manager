728d986a9c8a231d51f2ea46a5af0ada7da314f5
Matthew <matthew.t.mckenney@gmail.com>
2026-05-01 12:55:09 -0600

    created folders and task.h

---
+++ src/Task.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <chrono>
+
+class Task {
+    public:
+        enum class Status{
+            Pending,
+            Done
+        };
+
+        Task(const std::string& title);
+
+        // Getters
+        const std::string& getTitle() const;
+        const std::string& getDescription() const;
+        Status getStatus() const;
+        std::string getId() const;
+
+        // Setters
+        void setDescription(const std::string& desc);
+        void markDone();
+        void markPending();
+
+    private:
+        std::string id;
+        std::string title;
+        std::string description;
+        Status status;
+        std::chrono::system_clock::time_point createAt;
+
+        // Helper to generate unique ID
+        statis std::string generateId();
+
+};
